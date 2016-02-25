56ddc0b63471686e62e0784ae4dbadb03b221297
Jakub Bereżański <kuba@berezanscy.pl>
2016-02-25 21:11:54 +0100

    VS2013.5: update metadata and logging
    Metadata:
    - fix software author (Microsoft)
    - fix project url
    - provide description
    - add tags: KB number and "admin" (per Chocolatey requirements)
    - add package maintainer (me)
    
    Code:
    - support spaces in TEMP path
    - add checksum
    - use https for download
    - reformat for readability

--- packages/VS2013.5/tools/chocolateyInstall.ps1
+++ packages/VS2013.5/tools/chocolateyInstall.ps1
@@ -1,3 +1,11 @@
-﻿Install-ChocolateyPackage `
-	'VS2013.5.exe' 'exe' "/Passive /NoRestart /Log $($env:temp)\VS20135.log" `
-	'http://download.microsoft.com/download/A/F/9/AF95E6F8-2E6E-49D0-A48A-8E918D7FD768/VS2013.5.exe' -validExitCodes @(0,3010)
+﻿$packageVersion = '1.0.0'
+$arguments = @{
+    packageName = 'VS2013.5'
+    url = 'https://download.microsoft.com/download/A/F/9/AF95E6F8-2E6E-49D0-A48A-8E918D7FD768/VS2013.5.exe'
+    checksumType = 'sha1'
+    checksum = '2E67C8D07447FDB3222D9F602AFAFAD3E60DC1CB'
+    installerType = 'exe'
+    silentArgs = "/Passive /NoRestart /Log ""${Env:TEMP}\${packageName}-${packageVersion}.log"""
+    validExitCodes = @(0, 3010)
+}
+Install-ChocolateyPackage @arguments
